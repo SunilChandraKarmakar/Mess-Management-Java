1a8b2797a63b8c8371105a301ec339da3cacfe71
Sunil Chandra Karmakar <sunil_karmakar@ymail.com>
2019-01-28 23:11:15 +0600

    Working Member, Seaving and Cost from

---
+++ src/mess_management/Mess_Management.java
@@ -0,0 +1,10 @@
+package mess_management;
+
+public class Mess_Management {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmMember fm = new frmMember();
+        fm.setVisible(true);
+    }
+    
+}
